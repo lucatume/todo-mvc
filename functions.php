63285efa82ce8182feafa24feb0cf70daa0dc14f
Luca Tumedei <luca@theaveragedev.com>
2015-09-03 12:07:23 +0200

    split functions into sub files

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+global $todomvc;
+$todomvc = new stdClass();
+
+/**
+ * Requirements
+ */
+include 'functions/requirements.php';
+
+/**
+ *  Scripts
+ */
+include 'functions/scripts.php';
+
+/**
+ * Routes
+ */
+include 'functions/routes.php';
